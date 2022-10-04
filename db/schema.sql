90829dfbab06640defd7b8f2dc07e84b4f525589
Will <wrightw404@gmail.com>
2022-10-04 15:04:59 -0700

    created foreign key calls to link together the tables through primary keys

--- db/schema.sql
+++ db/schema.sql
@@ -12,6 +12,7 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
 CREATE TABLE employee (
     employee_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -19,4 +20,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL, 
     manager_id INT 
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(role_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
 );
