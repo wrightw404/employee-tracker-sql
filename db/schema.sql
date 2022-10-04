7a093d770b43233440607c0403b2dc38fbf32ea2
Will <wrightw404@gmail.com>
2022-10-03 22:15:16 -0700

    finshed creating tables for department, role and employee in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,21 @@ DROP DATABASE IF EXISTS employeeTracker_db;
 CREATE DATABASE employeeTracker_db; 
 
 USE employeeTracker_db; 
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL 
+);
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
+);
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL, 
+    manager_id INT 
+);
