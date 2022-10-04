0940f9f30e1801244566a57560b61af3248c12f2
Will <wrightw404@gmail.com>
2022-10-03 23:09:14 -0700

    created role and employee values in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,27 @@
 USE employeeTracker_db 
 
 INSERT INTO department (name)
-VALUES ('Research'), ('Marketing'), ('Human Resources'), ('IT'), ('Operations')
+VALUES ('Front of House'), ('Back of House'), ('Human Resources'), ('IT'), ('Executive')
 
-INSERT INTO role 
-VALUES
+INSERT INTO role (title, department_id, salary)
+VALUES ('Chief Executive Officer', 5, 500000)
+('Senior Analyst', 5, 125000)
+('Programmer', 4, 100000)
+('Jr. Developer', 4,  75000)
+('HR Assistant', 3,  50000)
+('Executive Chef', 2,  60000)
+('Sous Chef', 2,  45000)
+('General Manager', 1,  70000)
+('Reception', 1,  35000)
 
-INSERT INTO employee 
-VALUES
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Brandon', 'Beane', 5, NULL),
+('Sean', 'McDermott', 5, NULL),
+('Josh', 'Allen', 4, 5),
+('Stefon', 'Diggs', 4, 5),
+('Tyler', 'Bass', 3, 5),
+('Micah', 'Hyde', 2, 5),
+('Jordan', 'Poyer', 2, 5),
+('Dawson', 'Knox', 1, 5),
+('TreMaine', 'Edmunds', 1, 5),
+
