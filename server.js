7f2ba0258711564df407a91184b265879933aa1d
Will <wrightw404@gmail.com>
2022-10-03 21:50:00 -0700

    finished startPrompt() to give you functions based on userInput

--- server.js
+++ server.js
@@ -30,7 +30,67 @@ function startPrompt(){
             name:'userInput',
             type:'list',
             message:'Welcome to employee tracker, please select what you would like to do',
-            choices:['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee(s) Role']
+            choices:
+            [
+            'View All Departments', 
+            'View All Roles', 
+            'View All Employees', 
+            'Add A Department', 
+            'Add A Role', 
+            'Add An Employee', 
+            'Update An Employee(s) Role',
+            'Exit'
+            ]
         }
-    ])
+    ]).then((answer) => {
+        switch(answer.userInput){
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+
+            case 'Add A Department':
+                addADepartment();
+                break;
+            
+            case 'Add A Role':
+                addARole();
+                break;
+            
+            case 'Add An Employee':
+                addAnEmployee();
+                break;
+
+            case 'Update An Employee(s) Role':
+                updateAnEmployeeRole();
+                break;
+            
+            case 'Exit':
+                db.end();
+                break;
+        }
+    })
 }
+
+//viewAllDepartments function
+
+//viewAllRoles function
+
+//viewAllEmployees function
+
+//AddADepartment function
+
+//addARole function
+
+//addAnEmployee function
+
+//updateAnEmployeeRole function
+
+//exitFunction Function 
