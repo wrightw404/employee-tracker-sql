117e2a37e0d2cd96f9b1119d52b0a74c8dc19ec2
Will <wrightw404@gmail.com>
2022-10-03 22:42:42 -0700

    created seeds for department in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE employeeTracker_db 
+
+INSERT INTO department (name)
+VALUES ('Research'), ('Marketing'), ('Human Resources'), ('IT'), ('Operations')
+
+INSERT INTO role 
+VALUES
+
+INSERT INTO employee 
+VALUES
