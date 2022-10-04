5c38b3a559c5b22de3e9d4ad305de39f78ddcbc9
Will <wrightw404@gmail.com>
2022-10-03 17:25:54 -0700

    created employeetracker database and updated server.js

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 const consoleTable = require('console.table');
 
+const PORT = process.env.PORT || 3002;
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.us(exress.json());
 
 const db = mysql.createConnection(
     {
       host: 'localhost',
       user: 'root',
       password: 'JESUITlax22',
-      database: 'books_db'
+      database: 'employeeTracker_db'
     },
-    console.log(`Connected to the books_db database.`)
+    console.log(`Connected to the employeeTracker_db database.`)
   );
   
 //create prompt
