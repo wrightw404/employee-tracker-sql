a370f67d15428d9711dff56ddf0832a07fb72c8e
Will <wrightw404@gmail.com>
2022-10-04 20:37:50 -0700

    got addADepartment() to function properly

--- server.js
+++ server.js
@@ -102,11 +102,7 @@ function viewAllRoles(){
         startPrompt();
     })
 }
-//viewAllEmployees function
-// LEFT JOIN employee manager on manager.id = employee.manager_id
-//     INNER JOIN department ON (department.department_id = role.department_id)
-//     ORDER BY employee.employee_id
-//INNER JOIN role ON (role.id = employee.role_id)
+
 function viewAllEmployees(){
     db.query(`SELECT employee.employee_id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
     FROM employee 
@@ -123,7 +119,24 @@ function viewAllEmployees(){
 }
 //AddADepartment function
 function addADepartment(){
-
+    //inquirer prompt
+    inquirer.prompt([
+        {
+            name: 'addDepartment',
+            type: 'input',
+            message: 'What would you like the name of the Department to be?'
+        }
+    ]).then((userInput) => {
+        db.query(`INSERT INTO department SET ?`, 
+        {
+            name: userInput.addDepartment
+        },
+        (err, res) => {
+            if(err) throw err;
+            console.log(`Congratulations! ${userInput.addDepartment} was added to Departments`)
+            startPrompt();
+        })
+    })
 }
 //addARole function
 function addARole(){
